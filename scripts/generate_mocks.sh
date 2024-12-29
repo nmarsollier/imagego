42378f1df788fd72b22d0725523a39e90cc0c5a3
Nestor Marsollier <nmarsollier@gmail.com>
2024-12-29 10:40:28 -0300

    scripts

--- scripts/generate_mocks.sh
+++ scripts/generate_mocks.sh
@@ -1,5 +1,7 @@
 rm -rf ./test/mockgen
 
+set -e
+
 mockgen -source=./internal/image/repository.go -destination=./test/mockgen/image_repository_mocks.go -package=mockgen
 mockgen -source=./internal/image/service.go -destination=./test/mockgen/image_service_mocks.go -package=mockgen
 mockgen -source=./internal/rabbit/consume_logout.go -destination=./test/mockgen/consume_logout_mocks.go -package=mockgen
