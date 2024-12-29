414bccb2ba40f7ddc974294db12b7feb9d6af88e
Nestor Marsollier <nmarsollier@gmail.com>
2024-12-29 12:42:26 -0300

    package

--- internal/graph/server.go
+++ internal/graph/server.go
@@ -1,4 +1,4 @@
-package server
+package graph
 
 import (
 	"fmt"
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"github.com/nmarsollier/commongo/log"
 	"github.com/nmarsollier/imagego/internal/di"
 	"github.com/nmarsollier/imagego/internal/env"
-	server "github.com/nmarsollier/imagego/internal/graph"
+	"github.com/nmarsollier/imagego/internal/graph"
 	"github.com/nmarsollier/imagego/internal/rest"
 )
 
@@ -18,7 +18,7 @@ import (
 // @host		localhost:3001
 // @BasePath	/v1
 func main() {
-	go server.Start()
+	go graph.Start()
 
 	di.NewInjector(log.Get(env.Get().FluentUrl, "imagego")).ConsumeLogoutService().Init()
 
